e3aa7a7c2d038f4b0f0d2e8d254d8b21a7942f3c
Jason Wilson <jason.wilson@ef.com>
2018-12-20 11:05:42 +0100

    feat(card): Localisation Add locale sniffing with os-locale Add translations for title and office fields Add office field Tweak colours

--- index.js
+++ index.js
@@ -3,27 +3,49 @@
 const boxen = require('boxen');
 const chalk = require('chalk');
 const {stripIndent} = require('common-tags');
+const locale = require('os-locale');
 
-console.log(
-	boxen(
-		stripIndent`
-			${chalk.blue.dim('Jason Wilson')}
-			${chalk.blue.bold('Senior Frontend Developer')}
-			${chalk.blue('Cultural Care AuPair / Education First')}
-			
-			${chalk.gray('https://www.wilsons.io')}
-			${chalk.gray('https://twitter.com/wilsonuponsea')}
-			${chalk.gray('https://www.github.com/sgiobairog')}
-		`,
-		{
-			borderColor: "blue",
-			borderStyle: "double",
-			dimBorder: true,
-			margin: 1,
-			padding: 1,
-			align: "center"
-		}
-	)
-);
+const t10s = {
+	'en' : {
+		'title': 'Senior Frontend Developer',
+		'office': 'Lucerne, Switzerland'
+	},
+	'de' : {
+		'title': 'Senior Frontend-Entwickler',
+		'office': 'Luzern, Schweiz'
+	}
+}
+
+const displayCard = ( lang = 'en' ) => {
+	lang = lang.substring(0,2);
+
+	console.log(
+		boxen(
+			stripIndent`
+				${chalk.magenta('Jason Wilson')}
+				${chalk.blue.bold(t10s[lang].title)}
+				${chalk.blue('Cultural Care AuPair / Education First')}
+				${chalk.gray(t10s[lang].office)}
+
+				${chalk.gray('https://www.wilsons.io')}
+				${chalk.gray('https://twitter.com/wilsonuponsea')}
+				${chalk.gray('https://www.github.com/sgiobairog')}
+			`,
+			{
+				borderColor: "blue",
+				borderStyle: "double",
+				dimBorder: true,
+				margin: 1,
+				padding: 1,
+				align: "center"
+			}
+		)
+	);
+}
+
+(async () => {
+	const lang = await locale({spawn: false});
+	displayCard(lang);
+})();
 
 
