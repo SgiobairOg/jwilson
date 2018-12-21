c0281a791dadb2699957dc785e7116a9ad62bf3e
Jason Wilson <jason.wilson@ef.com>
2018-12-21 11:17:56 +0100

    feat(card): Add Clickable links Add terminal-link package Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const boxen = require('boxen');
 const chalk = require('chalk');
 const {stripIndent} = require('common-tags');
 const locale = require('os-locale');
+const link = require('terminal-link');
 
 const t10s = {
 	'en' : {
@@ -18,6 +19,8 @@ const t10s = {
 	}
 }
 
+const urlAsText = (text, url) => text;
+
 const displayCard = ( lang = 'en' ) => {
 	lang = lang.substring(0,2);
 
@@ -27,14 +30,14 @@ const displayCard = ( lang = 'en' ) => {
 				${chalk.magenta('Jason Wilson')}
 				${chalk.blue.bold(t10s[lang].title)}
 				${chalk.blue('Cultural Care AuPair / Education First')}
-				${chalk.gray('jason.wilson@ef.com')}
+				${chalk.gray(link('jason.wilson@ef.com','mailto:jason.wilson@ef.com?subject=NPX%20Contact', {fallback: urlAsText}))}
 				${chalk.gray(t10s[lang].office)}
 
-				${chalk.gray('https://www.wilsons.io')}
-				${chalk.gray('https://twitter.com/wilsonuponsea')}
-				${chalk.gray('https://www.github.com/sgiobairog')}
+				${chalk.gray(link('https://www.wilsons.io', 'https://www.wilsons.io', {fallback: urlAsText}))}
+				${chalk.gray(link('https://twitter.com/wilsonuponsea', 'https://twitter.com/wilsonuponsea', {fallback: urlAsText}))}
+				${chalk.gray(link('https://www.github.com/sgiobairog','https://www.github.com/sgiobairog', {fallback: urlAsText}))}
 
-				${chalk.blue(`${t10s[lang].hiring} http://app.jobvite.com/m?3YMLbkwv`)}
+				${chalk.blue(`${t10s[lang].hiring} ${link('Frontenders and more.','http://app.jobvite.com/m?3YMLbkwv')}`)}
 			`,
 			{
 				borderColor: "blue",
